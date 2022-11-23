c1e0b88871640a30cae83810043f971e7ccdaeb1
dafizcool12 <dafizcool12@gmail.com>
2022-11-23 18:44:26 +0700

    fix: auto refresh api

--- components/List.js
+++ components/List.js
@@ -5,12 +5,16 @@ export default function List() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch(`https://www.scorebat.com/video-api/v3/feed?token=${process.env.NEXT_PUBLIC_API_TOKEN}`)
-      .then((response) => response.json())
-      .then((response) => setData(response.response))
-      .catch((err) => console.error(err));
+    setTimeout(() => {
+      fetch(
+        `https://www.scorebat.com/video-api/v3/feed?token=${process.env.NEXT_PUBLIC_API_TOKEN}`
+      )
+        .then((response) => response.json())
+        .then((response) => setData(response.response))
+        .catch((err) => console.error(err));
+    }, 250);
   });
-  
+
   return (
     <section class="text-gray-600 body-font">
       <div class="container px-5 py-8 mx-auto">
@@ -19,19 +23,19 @@ export default function List() {
             <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
               Watch football from all over the world
             </h1>
-            <div class="h-1 w-20 bg-indigo-500 rounded"></div>
+            <div class="h-1 w-20 bg-gray-500 rounded"></div>
           </div>
           <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">
-            Missed your favourite football team recent match? Dont worry! 
-            Search the match you want to watch below from hundreds of recent matches.
+            Missed your favourite football team recent match? Dont worry! Search
+            the match you want to watch below from hundreds of recent matches.
           </p>
         </div>
         <div class="flex flex-wrap -m-4">
-          {
-            data === null ?
-            (<p class="mx-auto text-center font-bold">Loading...</p>) :
-            data.map((item, index) => (<ItemCard key={index} data={item} />))
-          }
+          {data === null ? (
+            <p class="mx-auto text-center font-bold">Loading...</p>
+          ) : (
+            data.map((item, index) => <ItemCard key={index} data={item} />)
+          )}
         </div>
       </div>
     </section>
